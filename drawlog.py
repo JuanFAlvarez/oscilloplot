6d96d8f0301c976b8e8f6b813c46daa383c005c9
Juan Alvarez <65520250+JuanFAlvarez@users.noreply.github.com>
2021-05-23 05:59:21 -0500

    Update drawlog.py

--- drawlog.py
+++ drawlog.py
@@ -1,16 +1,18 @@
 import tkinter as tk
+# prints raw x and y to be processed
+
 X = []
 Y = []
 lastx, lasty = 0, 0
 
 def xy(event):
-    "Takes the coordinates of the mouse when you click the mouse"
+    # Takes the coordinates of the mouse when you click the mouse
     global lastx, lasty
     lastx, lasty = event.x, event.y
 
 def addLine(event):
-    """Creates a line when you drag the mouse
-    from the point where you clicked the mouse to where the mouse is now"""
+    # reates a line when you drag the mouse
+    # from the point where you clicked the mouse to where the mouse is now
     global lastx, lasty
     canvas.create_line((lastx, lasty, event.x, event.y))
     # this makes the new starting point of the drawing
@@ -30,12 +32,15 @@ root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 canvas = tk.Canvas(root)
 canvas.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+
+# bind left click and drag to functions and start loop
 canvas.bind("<Button-1>", xy)
 canvas.bind("<B1-Motion>", addLine)
 root.bind("<B1-Motion>",on_move_press)
 root.mainloop()
-print(" #define NUM_POINTS %s;" % str(len(X)))
-print("byte x_points[NUM_POINTS] = {%s};" % ','.join(X))
-print("byte y_points[NUM_POINTS] = {%s};" % ','.join(Y))
 
-#call python drawlog.py > swag.txt
+print("const int NUM_POINTS = %s;" % str(len(X)))
+print("const unsigned long x_points[NUM_POINTS] = {%s};" % ','.join(X))
+print("const unsigned long y_points[NUM_POINTS] = {%s};" % ','.join(Y))
+
+#call python drawlog.py > arduino_list.txt
